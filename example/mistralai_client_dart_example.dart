491602ac0ac604fdbe6d78a5b56b0715f8663c20
Marcin Gruchala <margruchala@gmail.com>
2024-01-11 12:04:26 +0100

    Create dart package template

---
+++ example/mistralai_client_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:mistralai_client_dart/mistralai_client_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/mistralai_client_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mistralai_client_dart;
+
+export 'src/mistralai_client_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
