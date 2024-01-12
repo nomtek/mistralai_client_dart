ad9a7b755be205b9af518152b8fa621c6fda3c47
Kacper Kluka <k.kluka@nomtek.com>
2024-01-12 15:59:53 +0100

    Add example test to make CI pass

--- test/mistralai_client_dart_test.dart
+++ test/mistralai_client_dart_test.dart
@@ -1,5 +1,15 @@
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {});
+  group('A group of tests', () {
+    test('Example test', () {
+      // given
+
+      // when
+      final result = 2 + 2;
+
+      // then
+      expect(result, equals(4));
+    });
+  });
 }
