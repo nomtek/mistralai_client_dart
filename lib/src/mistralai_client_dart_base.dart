d05fdd2f6108c2f6d4897420ac410507794f0ed4
Marcin Gruchala <margruchala@gmail.com>
2024-01-12 12:56:57 +0100

    Implement list models method + abstract get request function

--- lib/src/mistralai_client_dart_base.dart
+++ lib/src/mistralai_client_dart_base.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:http/retry.dart' as retry;
 import 'package:mistralai_client_dart/src/models/models.dart';
+
+class _MistralAPIEndpoints {
+  static const String listModels = '/v1/models';
+}
 
 class MistralAIClient {
   MistralAIClient({
@@ -19,8 +24,38 @@ class MistralAIClient {
   final int maxRetries;
   final http.Client _client;
 
+  Future<String> _getRequest<T>(String endpoint) async {
+    final headers = <String, String>{
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $apiKey',
+    };
+
+    try {
+      final response = await _client
+          .get(Uri.parse('$baseUrl$endpoint'), headers: headers)
+          .timeout(timeout);
+
+      if (response.statusCode != 200) {
+        throw Exception('Get request failed: ${response.statusCode}');
+      }
+
+      return response.body;
+    } catch (e) {
+      throw Exception('Get request failed: $e');
+    }
+  }
+
+  /// Returns a list of the available models [ModelsList] 
+  /// from the mistral AI API.
+  ///
+  /// It uses [list models endpoint](https://api.mistral.ai/v1/models) from the mistral AI API.
   Future<ModelsList> listModels() async {
-    throw UnimplementedError('to be implemented');
+    final response = await _getRequest<ModelsList>(
+      _MistralAPIEndpoints.listModels,
+    );
+
+    return ModelsList.fromJson(jsonDecode(response) as Map<String, dynamic>);
   }
 
   Future<ChatCompletion> chat(ChatParams params) {
