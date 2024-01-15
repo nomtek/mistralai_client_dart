e05c78622b8bcc580bcabd6b744ada24d635dd41
KacperKluka <62378170+KacperKluka@users.noreply.github.com>
2024-01-15 09:34:16 +0100

    Add GithubActions workflow for checking the code quality and tests (#14)
    * Add GithubActions workflow for checking the code quality and tests
    
    * Format code
    
    * Add example test to make CI pass
    
    * Fix test to make analyzer pass
    
    * Add check badge to the README
    
    * Adjust workflow so it works purely on dart

--- test/mistralai_client_dart_test.dart
+++ test/mistralai_client_dart_test.dart
@@ -2,5 +2,14 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
+    test('Example test', () {
+      // given
+
+      // when
+      const result = 2 + 2;
+
+      // then
+      expect(result, equals(4));
+    });
   });
 }
