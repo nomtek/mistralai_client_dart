c7dd26f6bc102e953e5b30da50a310a56b4707b1
Lukasz Gawron <gawronlucas@gmail.com>
2024-08-21 15:14:50 +0200

    remove old tests + add few simple tests to pipelines not failing due to lack of tests

---
+++ test/client_test.dart
@@ -0,0 +1,25 @@
+import 'package:mistralai_client_dart/mistralai_client_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Client', () {
+    test('should create a new instance of MistralAIClient', () {
+      // just check if doesn't throw
+      MistralAIClient(apiKey: 'apiKey');
+    });
+
+    test('should throw when base URL is not a http or https', () {
+      expect(
+        () => MistralAIClient(apiKey: 'apiKey', baseUrl: 'ws://api.mistral.ai'),
+        throwsA(isA<AssertionError>()),
+      );
+    });
+
+    test('should throw when base URL ends with /', () {
+      expect(
+        () => MistralAIClient(apiKey: 'apiKey', baseUrl: 'https://api.mistral.ai/'),
+        throwsA(isA<AssertionError>()),
+      );
+    });
+  });
+}
