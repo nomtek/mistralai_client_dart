0c903516e13c58fe5372669196e81c07d1f85663
Lukasz Gawron <gawronlucas@gmail.com>
2024-08-23 11:38:53 +0200

    add check for ci and api key in run_all_examples script

--- openapi_specs/run_all_examples.dart
+++ openapi_specs/run_all_examples.dart
@@ -3,6 +3,11 @@
 import 'dart:io';
 
 void main() async {
+  if (const bool.hasEnvironment('CI') &&
+      !const bool.hasEnvironment('EXAMPLES_MISTRAL_API_KEY')) {
+    stderr.writeln('EXAMPLES_MISTRAL_API_KEY is not set');
+    exit(1);
+  }
   final exampleDir = Directory('example');
   final exampleFiles = exampleDir.listSync();
   for (final file in exampleFiles) {
@@ -10,9 +15,9 @@ void main() async {
       print('running file: ${file.path}');
       final result = await Process.run('dart', ['run', file.path]);
       if (result.exitCode != 0) {
-        stderr.writeln(
-          'error: file: ${file.path}, exitCode: ${result.exitCode}',
-        );
+        stderr
+          ..writeln('error: file: ${file.path}, exitCode: ${result.exitCode}')
+          ..writeln(result.stderr);
         exit(result.exitCode);
       } else {
         stdout.writeln(
