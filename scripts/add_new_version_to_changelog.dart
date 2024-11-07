9653fdd0deef1379df2d8ff6ebe17980a8f9cb5f
Lukasz Gawron <gawronlucas@gmail.com>
2024-11-07 22:00:04 +0100

    fix: lint issues

--- scripts/add_new_version_to_changelog.dart
+++ scripts/add_new_version_to_changelog.dart
@@ -1,27 +1,27 @@
 import 'dart:io';
 
 String incrementVersion(String version) {
-  List<String> parts = version.split('.');
-  int patch = int.parse(parts[2]) + 1;
+  final parts = version.split('.');
+  final patch = int.parse(parts[2]) + 1;
   return '${parts[0]}.${parts[1]}.$patch';
 }
 
-void addNewChangelogSection(String filePath, String newChanges) async {
-  File file = File(filePath);
-  String content = await file.readAsString();
+Future<void> addNewChangelogSection(String filePath, String newChanges) async {
+  final file = File(filePath);
+  var content = await file.readAsString();
 
   // Find the latest version
-  RegExp versionExp = RegExp(r'## (\d+\.\d+\.\d+)');
-  Match? latestVersionMatch = versionExp.firstMatch(content);
+  final versionExp = RegExp(r'## (\d+\.\d+\.\d+)');
+  final Match? latestVersionMatch = versionExp.firstMatch(content);
   if (latestVersionMatch == null) {
     throw Exception('No version found in the changelog.');
   }
 
-  String latestVersion = latestVersionMatch.group(1)!;
-  String newVersion = incrementVersion(latestVersion);
+  final latestVersion = latestVersionMatch.group(1)!;
+  final newVersion = incrementVersion(latestVersion);
 
   // Create new changelog section
-  String newSection = '## $newVersion\n\n$newChanges\n\n';
+  final newSection = '## $newVersion\n\n$newChanges\n\n';
 
   // Insert new section at the top after the first heading
   content =
@@ -31,7 +31,7 @@ void addNewChangelogSection(String filePath, String newChanges) async {
 }
 
 void main() {
-  String newChanges =
+  const newChanges =
       '- New feature: Example feature\n- Improvement: Example improvement';
   addNewChangelogSection('CHANGELOG.md', newChanges);
 }
